f155d3fabfb16258396d2db5c5a3f255ec8f41ac
John M Dusel <jdusel@frederick.edu>
2025-10-19 08:03:56 -0400

    ENH: projects.bank: Driver code for phases 1-3

---
+++ src/projects/Bank/Main.java
@@ -0,0 +1,87 @@
+package projects.Bank;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+
+        public static void main(String[] args) {
+        phase1();
+        phase2();
+    }
+
+    public static void phase1() {
+        String logName = "data/phase1.log";
+        try {
+            FileWriter writer = new FileWriter(new File(logName));
+
+            Account acct = new Account(
+                "id1",
+                "Owner Name",
+                1.0,
+                AccountType.SAVINGS
+            );
+            
+            writer.write(
+                String.format(
+                    "Account setup: acct id=%s, owner=%s, balance=%f",
+                    acct.getID(),
+                    acct.getName(),
+                    acct.getBalance()
+                ) + System.lineSeparator()
+            );
+
+            Bank bank = new Bank();
+            int numAccounts0 = bank.getCount();
+            int findAcct0 = bank.find(acct.getID());
+
+            boolean addResult = bank.add(acct);
+            int numAccounts1 = bank.getCount();
+            int findAcct1 = bank.find(acct.getID());
+
+            writer.write(
+                String.format(
+                    "Bank init: getCount=%d, find=%d",
+                    numAccounts0, 
+                    findAcct0
+                ) + System.lineSeparator()
+            );
+
+            writer.write(
+                String.format(
+                    "After add: result=%b, getCount=%d, find=%d",
+                    addResult,
+                    numAccounts1, 
+                    findAcct1
+                ) + System.lineSeparator()
+            );
+
+            writer.close();
+        } catch (IOException e) { e.printStackTrace(); }
+
+    }
+
+    public static void phase2() {
+        String accountsFilename = "data/accounts.csv";
+        Bank bank = new Bank();
+        boolean result = bank.loadCSV(accountsFilename);
+
+        System.out.println("Result of loading account: " + result);
+        System.out.println("Number of accounts: " + bank.getCount());
+
+        String outputFilename = "data/phase2.csv";
+        result = bank.writeCSV(outputFilename);
+        System.out.println("Result of writing account: " + result);
+    }
+
+    public static void phase3() {
+        Bank bank = new Bank();
+        bank.loadCSV("data/accounts.csv"); // ignore output
+        boolean step2 = bank.processTransactions("data/transactions.csv");
+        boolean step3 = bank.writeCSV("data/accounts.csv");
+        System.out.println("Transactions process: " + step2);
+        System.out.println("Accounts write: " + step3);
+    }
+
+}
