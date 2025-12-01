91324094aafaa559d9d57e85946c49e1e97820c7
Ryan Gildersleeve <rcgildersleeve09@gmail.com>
2025-12-01 02:33:59 +0000

    Upd Project.maze - updated coords test

--- test/projects/Maze/CoordsTest.java
+++ test/projects/Maze/CoordsTest.java
@@ -6,18 +6,30 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CoordsTest {
-    
-
+    Coords coord1;
+    Coords coord2;
+    Coords coord3;
+    Coords coord4;
+    Coords coord5;
+    @BeforeEach
+    void setup(){
+        
+        coord1 = new Coords(1,4);
+        coord2 = new Coords(1,4);
+        coord3 = new Coords(1,2);
+        coord4 = new Coords(8,4);
+        coord5 = new Coords(5,7);
+    }
     @Test
     void testCoordsEquals(){
-        Coords coord1 = new Coords(1,4);
-        Coords coord2 = new Coords(1,4);
-        Coords coord3 = new Coords(1,2);
-        Coords coord4 = new Coords(8,4);
-        Coords coord5 = new Coords(5,7);
         assertTrue(coord1.equals(coord2));
         assertFalse(coord1.equals(coord3));
         assertFalse(coord1.equals(coord5));
         assertFalse(coord1.equals(coord4));
+    }
+    @Test
+    void testAccesors(){
+        assertEquals(8, coord4.getRow());
+        assertEquals(4, coord4.getCol());
     }
 }
